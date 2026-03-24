cd9f10bed9d4e4a9f39a6993385da8b6de5a53ba
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2026-03-24 15:01:25 +0900

    1-3-1 루트 레이아웃 구성

--- app/(trip)/index.tsx
+++ app/(trip)/index.tsx
@@ -1,61 +1,11 @@
-import { useDeferredValue, useMemo, useState } from 'react'
-import { FlatList, Text, TextInput } from 'react-native'
+import { theme } from '@/constants/theme'
+import { Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-function slowFilter(text: string) {
-  const result: string[] = []
-
-  for (let i = 0; i < 1500; i++) {
-    result.push(`${text} - item ${i}`)
-  }
-
-  return result
-}
-
 const MyTripList = () => {
-  const [textValue, setTextValue] = useState('')
-
-  // @useTransition 방식 (리렌더링을 지연시킴)
-  // const [items, setItems] = useState<string[]>([])
-  // const [isPending, startTransition] = useTransition()
-
-  // const handleChange = (value: string) => {
-  //   setTextValue(value)
-  //   startTransition(() => {
-  //     setItems(slowFilter(value))
-  //   })
-  // }
-
-  // @useDeferredValue 방식 (값의 업데이트를 지연시킴)
-  const deferredTextValue = useDeferredValue(textValue)
-
-  const items = useMemo(() => {
-    if (!deferredTextValue) return []
-    return slowFilter(deferredTextValue)
-  }, [deferredTextValue])
-
-  const isPending = textValue !== deferredTextValue
-
   return (
     <SafeAreaView>
-      <TextInput
-        value={textValue}
-        // onChangeText={handleChange}
-        onChangeText={setTextValue}
-        style={{ height: 50, borderWidth: 1 }}
-      />
-
-      {isPending && (
-        <Text style={{ color: 'gray' }}>리스트 업데이트 중...</Text>
-      )}
-
-      <FlatList
-        data={items}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => (
-          <Text style={{ opacity: isPending ? 0.5 : 1 }}>{item}</Text>
-        )}
-      />
+      <Text style={{ fontFamily: theme.fonts.bold }}>폰트 적용 확인</Text>
     </SafeAreaView>
   )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,27 +1,46 @@
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from '@react-navigation/native'
-import { Stack } from 'expo-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { useFonts } from 'expo-font'
+import { SplashScreen, Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
+import { useEffect } from 'react'
 import 'react-native-reanimated'
 
-import { useColorScheme } from '@/hooks/use-color-scheme'
+SplashScreen.preventAutoHideAsync()
 
 export const unstable_settings = {
   anchor: '(trip)',
 }
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 3,
+        staleTime: 1000 * 60 * 5,
+        gcTime: 1000 * 60 * 5,
+      },
+    },
+  })
+
+  const [fontsLoaded] = useFonts({
+    'Pretendard-Regular': require('../assets/fonts/Pretendard-Regular.otf'),
+    'Pretendard-Medium': require('../assets/fonts/Pretendard-Medium.otf'),
+    'Pretendard-SemiBold': require('../assets/fonts/Pretendard-SemiBold.otf'),
+    'Pretendard-Bold': require('../assets/fonts/Pretendard-Bold.otf'),
+  })
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontsLoaded])
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <QueryClientProvider client={queryClient}>
       <Stack>
         <Stack.Screen name='(trip)' options={{ headerShown: false }} />
       </Stack>
       <StatusBar style='auto' />
-    </ThemeProvider>
+    </QueryClientProvider>
   )
 }
